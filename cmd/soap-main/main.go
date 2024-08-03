936215af7e766033c25cb5898f1a1d6134decee6
Andrea Cavallo <67713825+Andrea-Cavallo@users.noreply.github.com>
2024-08-03 19:36:14 +0200

    aggiunta doc

--- cmd/soap-main/main.go
+++ cmd/soap-main/main.go
@@ -10,11 +10,10 @@ import (
 	"time"
 )
 
-func init() {
-	log.SetFlags(0)
-	log.SetOutput(os.Stdout)
-	log.SetPrefix("••••••• ✈️✈️ ")
-}
+// main is the entry point of the program. It initializes the CountryInfoService, performs two SOAP API calls
+// using different contexts, and prints the list of continents returned by each API call.
+// The first API call is made without a context, and the second API call is made with a timeout context of 5 seconds.
+// The program also logs debug information before and after each API call.
 func main() {
 	countryInfoService := initializeCountryInfoService()
 	log.Println("Prima chiamata senza context")
@@ -26,6 +25,21 @@ func main() {
 	countryInfoService.ListOfContinentsByName(ctx)
 }
 
+// init is called implicitly at program startup. It sets the log flags, log output,
+// and the log prefix.
+// It sets the log flags to 0, so that no additional information is included in the log messages.
+// It sets the log output to os.Stdout, so that log messages are written to the standard output.
+// It sets the log prefix to "••••••• ✈️✈️ ", which is added at the beginning of each log message.
+func init() {
+	log.SetFlags(0)
+	log.SetOutput(os.Stdout)
+	log.SetPrefix("••••••• ✈️✈️ ")
+}
+
+// initializeCountryInfoService creates a new instance of the CountryInfoService by initializing all its dependencies.
+// It retrieves the configuration from NewConfig, creates a CountryInfoClient using NewCountryInfoClient,
+// and finally initializes the CountryInfoService using NewCountryInfoService.
+// The initialized CountryInfoService is returned.
 func initializeCountryInfoService() *service.CountryInfoService {
 	configuration := config.NewConfig()
 	soapClient := client.NewCountryInfoClient(configuration.CountryInfoServiceURL)
--- internal/client/country_info_client.go
+++ internal/client/country_info_client.go
@@ -8,7 +8,6 @@ import (
 	"github.com/hooklift/gowsdl/soap"
 )
 
-// CountryInfoClient è il client per interagire con il servizio SOAP di informazioni sui paesi.
 type CountryInfoClient struct {
 	soapClient *soap.Client
 }
@@ -24,20 +23,50 @@ func NewCountryInfoClient(url string) *CountryInfoClient {
 	}
 }
 
+// makeSoapCall esegue una chiamata SOAP al servizio specificato usando il client SOAP sottostante.
+// Parametri:
+// - ctx: il contesto della chiamata.
+// - endpointURL: l'URL dell'endpoint del servizio.
+// - request: la struttura di richiesta SOAP.
+// - response: la struttura di risposta SOAP.
+// Restituisce:
+// - Un eventuale errore che si è verificato durante la chiamata SOAP.
 func (cic *CountryInfoClient) makeSoapCall(ctx context.Context, endpointURL string, request, response interface{}) error {
 	return cic.soapClient.CallContext(ctx, endpointURL, request, response)
 }
 
+// makeSoapCallWithoutContext esegue una chiamata SOAP al servizio specificato
+// usando il client SOAP sottostante.
+//
+// Parametri:
+// - endpointURL: l'URL dell'endpoint del servizio.
+// - request: la struttura di richiesta SOAP.
+// - response: la struttura di risposta SOAP.
+//
+// Restituisce:
+// - Un eventuale errore che si è verificato durante la chiamata SOAP.
 func (cic *CountryInfoClient) makeSoapCallWithoutContext(endpointURL string, request, response interface{}) error {
 	return cic.soapClient.Call(endpointURL, request, response)
 }
 
+// ListOfContinentsByName esegue una chiamata SOAP al servizio ListOfContinentsByName utilizzando il client del servizio CountryInfo.
+// Parametri:
+// - ctx: il contesto della chiamata.
+// - request: la struttura di richiesta ListOfContinentsByName.
+// Restituisce:
+// - la struttura di risposta ListOfContinentsByNameResponse.
+// - un eventuale errore che si è verificato durante la chiamata SOAP.
 func (cic *CountryInfoClient) ListOfContinentsByName(ctx context.Context, request *generated.ListOfContinentsByName) (*generated.ListOfContinentsByNameResponse, error) {
 	response := new(generated.ListOfContinentsByNameResponse)
 	err := cic.makeSoapCall(ctx, constants.ListOfContinentsByNameURL, request, response)
 	return response, err
 }
 
+// ListOfContinentsByNameWithoutContext esegue una chiamata SOAP al servizio ListOfContinentsByName
+// senza l'utilizzo di un contesto. Restituisce la struttura di risposta ListOfContinentsByNameResponse
+// e un eventuale errore che si è verificato durante la chiamata SOAP.
+// Parametri:
+// - request: la struttura di richiesta ListOfContinentsByName.
 func (cic *CountryInfoClient) ListOfContinentsByNameWithoutContext(request *generated.ListOfContinentsByName) (*generated.ListOfContinentsByNameResponse, error) {
 	response := new(generated.ListOfContinentsByNameResponse)
 	err := cic.makeSoapCallWithoutContext(constants.ListOfContinentsByNameURL, request, response)
